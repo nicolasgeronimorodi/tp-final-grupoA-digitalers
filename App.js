8665d30378ea4a07f440baaed999e44a844d00fd
Nicolas Rodi <nicolasgeronimorodi@gmail.com>
2021-10-20 19:20:55 -0300

    esqueleto fron-end

---
+++ App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import AppContainer from './components/AppContainer';
+
+function App() {
+  return (
+    <div className="App">
+      <AppContainer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ components/AppContainer.js
@@ -0,0 +1,22 @@
+import React from "react"
+import Header from "./Header"
+import LinkForm from "./LinkForm"
+
+const AppContainer=()=>{
+    return (
+        <>
+        <Header/>
+        <div className="container">
+            
+        <h1> Bienvenido a bitlyclone   </h1>
+        
+        <LinkForm />
+        </div>
+        </>
+        
+    )
+
+
+
+}
+export default AppContainer
